d9a83ba4c13f191653621ed1417cef3f7812d4d3
Pyalova Liza <liza.pyalllova@gmail.com>
2022-11-09 10:59:07 +0300

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
 
-
-
 public class Main {
+    public static double sum(double a, double b){
+        return a+b;
+    }
+    public static double differ(double a, double b){
+        return a-b;
+    }
+    public static double multipl(double a, double b){
+        return a*b;
+    }
+    public static double div(double a, double b){
+        return a/b;
+    }
     public static double Calculator(double num1, double num2, char op){
         switch(op) {
-            case '+': return num1 + num2;
-            case '-': return num1 - num2;
-            case '*': return num1 * num2;
-            case '/': return num1 / num2;
-            default:  System.out.printf("Error! Enter correct operator");
-                return -1.0;
+            case '+': return sum(num1, num2); //Design Pattern Facade
+            case '-': return differ(num1, num2); //DPF
+            case '*': return multipl(num1, num2); //DPF
+            case '/': return div(num1, num2); //DPF
+            default:  System.out.println("Error! Enter correct operator");
+                return -1.1111111111111;
         }
     }
     public static void main(String[] args) {
                 double num1;
                 double num2;
-                double ans;
                 char op;
                 Scanner reader = new Scanner(System.in);
                 System.out.print("Enter two numbers: ");
@@ -24,6 +33,7 @@ public class Main {
                 num2 = reader.nextDouble();
                 System.out.print("\nEnter an operator (+, -, *, /): ");
                 op = reader.next().charAt(0);
-                System.out.printf(num1 + " " + op + " " + num2 + " = " + Calculator(num1, num2, op));
+                double res = Calculator(num1, num2, op); //DPF
+                System.out.println(num1 + " " + op + " " + num2 + " = " + res);
     }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -19,4 +19,46 @@ class MainTest {
         double res=Main.Calculator(8.0, 2.0, '*');
         assertEquals(16.0, res);
     }
+
+    @Test
+    void sum() {
+        assertEquals(5, Main.sum(1,4));
+    }
+
+    @Test
+    void differ() {
+        assertEquals(7, Main.differ(8,1));
+    }
+
+    @Test
+    void differ1() {
+        assertEquals(9.7, Main.differ(10,0.3));
+    }
+
+    @Test
+    void differ2() {
+        assertEquals(8.8, Main.differ(10,1.2));
+    }
+
+    @Test
+    void multipl() {
+        assertEquals(80, Main.multipl(5, 16));
+    }
+
+    @Test
+    void multipl1() {
+        assertEquals(11, Main.multipl(5.5, 2));
+    }
+
+
+    @Test
+    void div() {
+        assertEquals(5, Main.div(10, 2));
+    }
+
+    @Test
+    void div1() {
+        assertEquals(6.5, Main.div(13, 2));
+    }
+
 }
